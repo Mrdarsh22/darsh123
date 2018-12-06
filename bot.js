bcc8425e8f452ca58fea3fc8b513b72ccdf03ece
Mrdarsh22 <45404097+Mrdarsh22@users.noreply.github.com>
2018-12-07 01:48:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,6 +151,14 @@ client.on('message', message => {
 });
 
 
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply("Almost Here!");
+    }
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
