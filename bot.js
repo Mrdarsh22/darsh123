486bb944c73414ac01450500d16027e247c74c71
Mrdarsh22 <45404097+Mrdarsh22@users.noreply.github.com>
2018-12-06 14:57:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,21 @@ client.on('guildMemberAdd', member => { //metas -HONRAR-
   }); 
 })
 
+client.on('message', async msg => {
+const devs = ['444840274267602944'];
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = "$";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
 
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
 
 
 
