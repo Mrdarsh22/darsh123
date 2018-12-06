5633a1b58677b37a95da4e9641de73bdaaaad935
Mrdarsh22 <45404097+Mrdarsh22@users.noreply.github.com>
2018-12-06 15:06:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,16 +31,6 @@ client.user.setGame(`DARSH SAD  `,"https://www.twitch.tv/dggamingbot")
 });
 
 
-client.on('guildMemberAdd', member => { //metas -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "💢general💢");
-     stewart.send(`<@${member.user.id}> invites by, <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
-})
 
         var prefix = "$";
     client.on('message', message => {
