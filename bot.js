61742feca273b4e06049d6e79b9284c4f1e577b9
Mrdarsh22 <45404097+Mrdarsh22@users.noreply.github.com>
2018-12-06 15:15:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,25 +30,27 @@ client.user.setGame(`DARSH SAD  `,"https://www.twitch.tv/dggamingbot")
   console.log('')
 });
 
-        var prefix = "$";
-    client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "spam") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR**');
-
-
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
+client.on('message', message => {
+    var args = message.content.toLowerCase().split(' ');
+    var command = args[0];
+    var prefix = '+';
+    var wordsSay = message.content.split(' ').slice(1).join(' ');
+    
+    if(command == prefix + 'say') {
+        var sayRole = message.guild.roles.find(r => r.name === 'say'); 
+        if(!sayRole) return message.channel.send('i cant find with u `say prmt` ');
+        if(!message.member.roles.has(sayRole.id)) return message.channel.send('u should have `say prmt`');
+        if(!wordsSay) return message.channel.send(`***EX :*** ${prefix}say Hello World! `);
+        
+        message.delete();
+        let sayE = new Discord.RichEmbed()  
+        .setColor('RANDOM')
+        .setDescription(`**${wordsSay}**`)  
+        
+        message.channel.send(sayE);
+    }
+});
 
 
 
