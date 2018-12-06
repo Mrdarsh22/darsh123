f987798482d255ba2e1a9dd694025fa4b6b80bb5
Mrdarsh22 <45404097+Mrdarsh22@users.noreply.github.com>
2018-12-06 15:35:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,4 +51,26 @@ client.user.setGame(`DARSH SAD  `,"https://www.twitch.tv/dggamingbot")
       });
 
 
+client.on('message', message => {
+if(message.content.startsWith("$slots")) {
+let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slot2 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slot3 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+let we;
+if(slots1 === slots2 && slots2 === slots3) {
+we = "Win, GG."
+} else {
+we = "Lose :'("
+}
+message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
 client.login(process.env.BOT_TOKEN);
